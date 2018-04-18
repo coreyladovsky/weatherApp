ff2253f80bcdaf77c9e0578a741736383b975b00
Corey Ladovsky <coreyskymusic@gmail.com>
2018-04-18 14:39:36 -0400

    added favicon

--- weather.js
+++ weather.js
@@ -1,3 +1,6 @@
+var mykey = config.MY_KEY;
+var secretkey = config.SECRET_KEY;
+
 const cleanerDate = (dateString) => {
   return dateString.slice(0, 10);
 };
@@ -5,7 +8,7 @@ const cleanerDate = (dateString) => {
 
 document.addEventListener("DOMContentLoaded", ()=> {
   $.ajax({
-    url: "http://api.aerisapi.com/forecasts/11101?client_id=IAK2P95sLPnNNvlTaAG2w&client_secret=T774l7LXjlO4JxVafT2jEJDg6ZQYkpvy7Yv12HHK",
+    url: `http://api.aerisapi.com/forecasts/11101?client_id=${mykey}&client_secret=${secretkey}`,
     method: "GET"
   }).then((res) => {
     let data = res.response[0].periods;
