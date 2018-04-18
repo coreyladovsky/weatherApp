da839d2b0e19b2620b7b82060cb1ebdc4ca70715
Corey Ladovsky <coreyskymusic@gmail.com>
2018-04-18 15:54:02 -0400

    seperated out a function

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 var mykey = config.MY_KEY;
 var secretkey = config.SECRET_KEY;
 
+
 const cleanerDate = (dateString) => {
   let date = dateString.slice(0, 10);
   return date.slice(5) +  "-" + date.slice(0, 4);
@@ -8,7 +9,6 @@ const cleanerDate = (dateString) => {
 
 
 document.addEventListener("DOMContentLoaded", ()=> {
-  let tempF = true;
   $.ajax({
     url: `http://api.aerisapi.com/forecasts/11101?client_id=${mykey}&client_secret=${secretkey}`,
     method: "GET"
@@ -18,44 +18,45 @@ document.addEventListener("DOMContentLoaded", ()=> {
     let celciusTemps = [];
     let fTemps = [];
     data.forEach((day, i) => {
-      let iconPath = "./icons/" + day.icon
-      let celciusMax = day.maxTempC + "° C"
-      let celciusMin = day.minTempC + "° C"
-      celciusTemps.push({celciusMax, celciusMin})
-      let fMax = day.maxTempF + "° F"
-      let fMin = day.minTempF + "° F"
-      fTemps.push({fMax, fMin})
-      let newDay = $("<div> </div>")
-      newDay.addClass(`.weekday weekday${i}`)
-      root.append( newDay )
+      let iconPath = "./icons/" + day.icon;
+      let celciusMax = day.maxTempC + "° C";
+      let celciusMin = day.minTempC + "° C";
+      celciusTemps.push({celciusMax, celciusMin});
+      let fMax = day.maxTempF + "° F";
+      let fMin = day.minTempF + "° F";
+      fTemps.push({fMax, fMin});
+      let newDay = $("<div> </div>");
+      newDay.addClass(`.weekday weekday${i}`);
+      root.append( newDay );
       newDay.append(`<div class="day-name">${cleanerDate(day.dateTimeISO)}</div>`);
       newDay.append(`<img class="icon" src=${iconPath} alt=""/>`);
       newDay.append(`<div class="day-max">High: ${fMax}</div>`);
       newDay.append(`<div class="day-min">Low: ${fMin}</div>`);
     });
 
+    let tempF = true;
     $(".toggle").on("click", () => {
       if(tempF) {
-        let allHighs = $(".day-max");
-        for (let i = 0; i < allHighs.length; i++) {
-          allHighs[i].innerHTML = "High: " +  celciusTemps[i].celciusMax;
-        }
-        let allLows = $(".day-min");
-        for (let i = 0; i < allLows.length; i++) {
-          allLows[i].innerHTML = "Low: " + celciusTemps[i].celciusMin;
-        }
+        changeTemps(celciusTemps);
       } else {
-          let allHighs = $(".day-max");
-          for (let i = 0; i < allHighs.length; i++) {
-            allHighs[i].innerHTML = "High: " +  fTemps[i].fMax;
-          }
-          let allLows = $(".day-min");
-          for (let i = 0; i < allLows.length; i++) {
-            allLows[i].innerHTML = "Low: " + fTemps[i].fMin;
-          }
+        changeTemps(fTemps, true);
       }
       tempF = !tempF;
     });
-    
+
   });
 });
+
+const changeTemps = (tempArray, f) => {
+  let allHighs = $(".day-max");
+  let allLows = $(".day-min");
+  for (let i = 0; i < allHighs.length; i++) {
+    if(f) {
+      allHighs[i].innerHTML = "High: " +  tempArray[i].fMax;
+      allLows[i].innerHTML = "Low: " + tempArray[i].fMin;
+    } else {
+      allHighs[i].innerHTML = "High: " +  tempArray[i].celciusMax;
+      allLows[i].innerHTML = "Low: " + tempArray[i].celciusMin;
+    }
+  }
+};
