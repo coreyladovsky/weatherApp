ab1639649a4183e9a67b682604fb803b62e85472
Corey Ladovsky <coreyskymusic@gmail.com>
2018-04-18 13:55:09 -0400

    initial commit, working ajax call and showing date and time

---
+++ weather.js
@@ -0,0 +1,27 @@
+const cleanerDate = (dateString) => {
+  return dateString.slice(0, 10);
+};
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+  $.ajax({
+    url: "http://api.aerisapi.com/forecasts/11101?client_id=IAK2P95sLPnNNvlTaAG2w&client_secret=T774l7LXjlO4JxVafT2jEJDg6ZQYkpvy7Yv12HHK",
+    method: "GET"
+  }).then((res) => {
+    let data = res.response[0].periods;
+    let root = $("#root");
+    data.forEach((day, i) => {
+      let newDay = $("<div> </div>")
+      newDay.addClass(`.weekday weekday${i}`)
+      root.append( newDay )
+      newDay.append(`<div class="day-name">${cleanerDate(day.dateTimeISO)}</div>`);
+      newDay.append(`<div class="day-min">${day.minTempF}</div>`);
+      newDay.append(`<div class="day-max">${day.maxTempF}</div>`);
+    });
+
+
+
+
+
+  });
+});
